09e9d4ad889f8e7b3a3b6eea64c2cd6e979b1f5d
twoh <hherdinaufal@gmail.com>
2017-01-14 23:35:28 +0700

    add edittext inputtype tutorial

--- src/main/java/id/web/twoh/twoh_android_layoutbasic/MainActivity.java
+++ src/main/java/id/web/twoh/twoh_android_layoutbasic/MainActivity.java
@@ -76,5 +76,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, TWOHLinearLayout.class));
             }
         });
+
+        Button btEditText = (Button) findViewById(R.id.bt_edittext);
+        btEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TWOHEditText.class));
+            }
+        });
     }
 }
