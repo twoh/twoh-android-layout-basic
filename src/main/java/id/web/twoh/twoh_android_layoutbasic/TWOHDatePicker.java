5b4409589a60d2b40bbb76750b2c412cd0195c99
twoh <hherdinaufal@gmail.com>
2017-01-21 10:39:33 +0700

    add datepicker sample

--- src/main/java/id/web/twoh/twoh_android_layoutbasic/TWOHDatePicker.java
+++ src/main/java/id/web/twoh/twoh_android_layoutbasic/TWOHDatePicker.java
@@ -1,10 +1,17 @@
 package id.web.twoh.twoh_android_layoutbasic;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by Herdi_WORK on 20.01.17.
@@ -12,20 +19,61 @@ import android.widget.Button;
 
 public class TWOHDatePicker extends AppCompatActivity {
 
-
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private TextView tvDateResult;
+    private Button btDatePicker;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datepicker);
 
-        Button btDatePicker = (Button) findViewById(R.id.bt_datepicker);
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+        tvDateResult = (TextView) findViewById(R.id.tv_dateresult);
+        btDatePicker = (Button) findViewById(R.id.bt_datepicker);
         btDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                showDateDialog();
             }
         });
 
+    }
+
+    private void showDateDialog(){
+
+        /**
+         * Calendar untuk mendapatkan tanggal saat ini
+         */
+        Calendar newCalendar = Calendar.getInstance();
+
+        /**
+         * Tampilkan DatePicker dialog
+         */
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                /**
+                 * Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
+                 */
+
+                /**
+                 * Set Calendar untuk menampung tanggal yang dipilih
+                 */
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                /**
+                 * Update TextView dengan tanggal yang kita pilih
+                 */
+                tvDateResult.setText("Tanggal dipilih : "+dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
     }
 }
